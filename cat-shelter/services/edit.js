9874c9c2ca684d85bab801b4f860e7ea85438f6d
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-09-19 16:30:58 +0300

    add cat shelter exercise

--- cat-shelter/services/edit.js
+++ cat-shelter/services/edit.js
@@ -1,12 +1,22 @@
 let formidable = require('formidable');
 let fs = require('fs');
+let uniqid = require('uniqid');
 
 let dbCats = require('../database/cats.json');
 
-function edit(req) {
-    let form  = formidable.IncomingForm()
+function edit(req, cat) {
+    let form = formidable.IncomingForm();
 
     form.parse(req, (err, fields, files) => {
+        fields.id = uniqid();
+
+        if (files['image']) {
+            fields.img = files.image.name;
+        }
+
+        dbCats.cats.push(fields);
+        let index = dbCats.cats.findIndex(c => c.id == cat.id)
+        dbCats.cats.splice(index, 1);
         let result = JSON.stringify(dbCats, '', 2);
         return fs.writeFileSync('./database/cats.json', result);
     });
