1af92770808f982e1f2015e7f4cfd7efeda0a687
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-09-25 17:25:23 +0300

    add search functionality

--- ExpressJS and Templating/cubicle-part 1/controllers/homeController.js
+++ ExpressJS and Templating/cubicle-part 1/controllers/homeController.js
@@ -1,6 +1,7 @@
 let cubes = require('../database/database.json');
 
 function homeRender(req, res) {
+    console.log(cubes);
     res.render('index', { cubes });
 }
 
@@ -9,7 +10,22 @@ function aboutRender(req, res) {
 }
 
 function search(req, res) {
+    let result = cubes;
 
+    if (req.query.search) {
+        result = result.filter(c => c.name.toLocaleLowerCase().includes(req.query.search.toLocaleLowerCase()));
+    }
+    
+    if (req.query.from) {
+        result = result.filter(c => c.difficulty >= req.query.from);
+    }
+
+    if (req.query.to) {
+        result = result.filter(c => c.difficulty <= req.query.to);
+    }
+
+    cubes = result;
+    res.render('index', { cubes });
 }
 
 module.exports = {
