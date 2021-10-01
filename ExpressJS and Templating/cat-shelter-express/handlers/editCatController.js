0808aa998ab3b230b4d4b4461f22dc4e1fc1ef9e
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-10-01 17:56:46 +0300

    add cat shelter exercise

--- ExpressJS and Templating/cat-shelter-express/handlers/editCatController.js
+++ ExpressJS and Templating/cat-shelter-express/handlers/editCatController.js
@@ -1,17 +1,20 @@
 let fs = require('fs');
 let express = require('express');
+let multer = require('multer');
 let cats = require('../database/cats.json');
 
 let router = express.Router();
+let upload = multer({ dest: "public/images/" });
 
 router.get('/cats-edit/:id?', (req, res) => {
     let cat = cats.find(c => c.id == req.params.id);
     res.render('editCat', { cat });
 });
 
-router.post('/cats-edit/:id?', (req, res) => {
+router.post('/cats-edit/:id?', upload.single('image'), (req, res) => {
     let cat = cats.find(c => c.id == req.params.id);
     let edittedCat = req.body
+    edittedCat.image = req.file.originalname;
     edittedCat.id = req.params.id;
     let result = JSON.stringify(cats).replace(JSON.stringify(cat), JSON.stringify(edittedCat));
     JSON.stringify(result, '', 2);
--- ExpressJS and Templating/cat-shelter-express/handlers/homeController.js
+++ ExpressJS and Templating/cat-shelter-express/handlers/homeController.js
@@ -8,13 +8,12 @@ router.get('/', (req, res) => {
 });
 
 router.get('/search', (req, res) => {
-    let result = cats.splice();
-
+    let result = cats.slice();
     result = cats.filter(c => c.breed.toLocaleLowerCase() == req.query.search.toLocaleLowerCase()
-        || c.name.toLocaleLowerCase() == req.query.search.toLocaleLowerCase());
+    || c.name.toLocaleLowerCase() == req.query.search.toLocaleLowerCase());
 
-    cats = result.splice();
-    res.render('index', { cats });
+    cats = result.slice();
+    res.render('index', { cats }); 
 });
 
 module.exports = router;
