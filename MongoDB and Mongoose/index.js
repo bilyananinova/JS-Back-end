15ef21da038577ffe2655bb5be0e9c884542d5ca
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-10-02 15:20:33 +0300

    initial commit

---
+++ MongoDB and Mongoose/index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+
+let config = require('./config/config.js')[process.env.NODE_ENV.trim()];
+require('./config/express.js')(app);
+require('./config/exphbs.js')(app);
+
+app.use(express.static(path.relative(__dirname, './static')));
+app.use(require('./router'));
+
+app.listen(config.PORT, () => {
+    console.log(`App is running on port ${config.PORT}...`);
+});
