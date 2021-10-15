ca4ccec3ce4e623d8404b970051e8e5cc6bd8529
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-10-15 22:53:47 +0300

    create validations

---
+++ Validation and Error Handling/cubicle-part 4/index.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+let cookieParser = require('cookie-parser');
+
+let port = 3000;
+
+require('./config/express.js')(app);
+require('./config/exphbs.js')(app);
+require('./config/database.js')(app);
+
+app.use(cookieParser());
+app.use(require('./middlewares/isLogged'));
+app.use(require('./routes'));
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}...`);
+});
