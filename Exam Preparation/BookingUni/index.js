c14828b97db1d00bd03f5141851efd72734d3920
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-10-25 23:54:55 +0300

    add BookingUni

---
+++ Exam Preparation/BookingUni/index.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let cookieParser = require('cookie-parser');
+let app = express();
+
+let { PORT } = require('./config/constants');
+let { auth } = require('./middlewares/auth');
+let router = require('./router');
+
+require('./config/express.js')(app);
+require('./config/exphbs.js')(app);
+require('./config/database.js')(app);
+
+app.use(cookieParser());
+app.use(auth);
+app.use(router);
+ 
+app.listen(PORT, () => {
+    console.log(`App is running on port http://localhost:${PORT}...`);
+});
