3a1444890536bd161b9ecb352e1df277a03a93a3
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2021-10-26 21:34:13 +0300

    Delete Exam Preparation/Theaters directory

--- Exam Preparation/Theaters/config/constants.js
+++
@@ -1,4 +0,0 @@
-exports.PORT = 3000;
-exports.CONNECTION_STRING = 'mongodb://localhost:27017/theaters'
-exports.SECRET = 'mySecret';
-exports.TOKEN = 'jwtoken'
--- Exam Preparation/Theaters/config/database.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-let { CONNECTION_STRING } = require('../config/constants');
-
-function initDb() {
-    return mongoose.connect(CONNECTION_STRING);
-}
-
-module.exports = initDb;
